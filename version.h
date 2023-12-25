82a70d4d70be3a6aafa8d1699aa21f08e1882d6f
Mike Tubby <mike@tubby.org>
2023-12-25 19:55:33 +0000

    Bump patch level

--- version.h
+++ version.h
@@ -7,7 +7,7 @@
 
 #define VERSION_MAJOR	2
 #define VERSION_MINOR	2
-#define VERSION_PATCH	7
+#define VERSION_PATCH	8
 
 #define COPYRIGHT       "Copyright (C) 2023 by Michael J. Tubby B.Sc. MIET G8TIC mike@tubby.org All Rights Reserved."
 
